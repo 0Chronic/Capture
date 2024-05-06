8281e2561f652c8cdcd8b033f798253a12bf7427
0Chronic <kyawn1063@gmail.com>
2024-05-06 23:33:21 +0630

    again

--- app/page.js
+++ app/page.js
@@ -7,6 +7,7 @@ export default function Home() {
        
       <h1>hello</h1>
       <h2>hello i did not do homework again</h2>
+      <h3>reminding not doing homework again fuck</h3>
       </div>
     </main>
   );
